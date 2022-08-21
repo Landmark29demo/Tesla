23b7c8dd5b8d2e1be65bb0b5edfe6e4a405f972a
Jane N <jnebo42@gmail.com>
2022-08-21 19:46:00 +0000

    Resolved 3

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
-<<<<<<< HEAD
-
-=======
 deploy java applications
 deploy python apps
 deploy in the dev environment
+janet
+dev
+56430c0 (conflict 2 resolved)
 deployment5 master
 deployment4
 deploy to staging environment
 echo good job
 tested and accepted
+(Tested and approved by the client QA team)
+56430c0 (conflict 2 resolved)
